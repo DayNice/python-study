6956e424a65786f6726ba42a94a99a330c5fc14b
DayNice <76981513+DayNice@users.noreply.github.com>
2021-09-15 14:16:28 +0900

    Add solutions

---
+++ src/baekjoon/class1/Sol11654.py
@@ -0,0 +1,7 @@
+def main():
+    lt = input()
+    print(ord(lt))
+
+
+if __name__ == "__main__":
+    main()
---
+++ src/baekjoon/class1/Sol1330.py
@@ -0,0 +1,12 @@
+def main():
+    a, b = map(int, input().split())
+    if a > b:
+        print(">")
+    elif a < b:
+        print("<")
+    else:
+        print("==")
+
+
+if __name__ == "__main__":
+    main()
